3628c5553d824e8c4a35ce6e8dc9c4bc607ddc6d
ClPhillips10 <Lakephillips3232@gmail.com>
2023-05-29 19:57:20 +0100

    Finished product

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,46 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            int i = 0;
+            while(i < 1)
+                    {
+                Console.WriteLine("Is your temp Farenheite (F) or Celcius(C)? ");
+                var user = Console.ReadLine();
+
+
+                //if user wants F to C
+                if (user.ToUpper() == "F")
+                {
+                    Console.Write("Enter the Temp in Farenheit to conver into Celcius: ");
+                    double Temp = double.Parse(Console.ReadLine());
+                    Console.WriteLine($"your conversion to celcius is {TempConverter.FarenheitToCelsius(Temp)}");
+                    Console.WriteLine(TempConverter.FarenheitToCelsius(Temp));
+                    //close the while loop
+                    i++;
+                }
+
+
+                //If user wants C to F
+
+                else if (user.ToUpper() == "C")
+                {
+                    Console.Write("Enter the Temp in Celcius to convert into Farenheit: ");
+                    double Temp = double.Parse(Console.ReadLine());
+                    Console.WriteLine($"your conversion to Farenheite is {TempConverter.CelsiusToFarenheit(Temp)}");
+                    Console.WriteLine(TempConverter.CelsiusToFarenheit(Temp));
+                    //Close the while loop 
+                    i++;
+                }
+
+
+                //if user did not enter c or f
+
+                else
+                {
+                    Console.WriteLine("You did not enter a valid conversion type. ");
+                }
+                Console.ReadLine();
+            }
+            }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace StaticExercise
+{
+    public class TempConverter
+    {
+        public static double FarenheitToCelsius(double farenheit)
+        {
+            var result = (farenheit - 32) / 1.8;
+            return result;
+        }
+
+
+
+        public static double CelsiusToFarenheit(double Celcius)
+        {
+            var result = (Celcius * 9) / 5 + 32;
+            return result;
+        }
+    }
+}
+
+
